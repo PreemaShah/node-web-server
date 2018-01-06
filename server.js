f091c01c0d7b8dc46c2293642a6efd3f1cae1e4f
preems <lanetteam.preema@gmail.com>
2018-01-05 19:44:28 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+    app=express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/static'));
+app.use((req,res,next)=>{
+
+    var now=new Date().toString();
+    var  log = `${now}: ${req.method} ${req.url}`;
+
+     console.log();
+     fs.appendFile('server.log',log +'\n');
+    next();
+})
+app.get('/',(req,res)=> {
+    res.render('home.hbs',
+    {
+        pageTitle:"Welcome to Home Page"
+
+    });
+});
+
+
+app.get('/about',(req,res)=>
+{
+    res.render('about.hbs',
+    {
+        pageTitle:"Welcome to About Page"
+
+    });
+});
+app.get('/bad',(req,res)=>
+{
+    res.send({
+    ErrorMessage:"This page Contains errors"
+});
+});
+app.listen(3000);
